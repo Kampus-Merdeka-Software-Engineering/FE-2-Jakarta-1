f787f72aac7a0f05929fc88eadcb461bcd68baec
nillaaayu <212410103050@mail.unej.ac.id>
2023-12-01 20:56:57 +0700

    first commit

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
+});
+ 
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  const windowPosition = window.scrollY > 0;
+  navBar.classList.toggle("scrolling-active", windowPosition);
+  menuNav.classList.remove("menu-active");
+});
